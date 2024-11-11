b1ea14e366486b0f18ee71e1f1b811f9e5415403
Syedamuntahashah <syedashah32399@gmail.com>
2024-11-11 22:08:02 +0500

    this my second project

--- my-portfolio2/src/components/About.tsx
+++ my-portfolio2/src/components/About.tsx
@@ -3,7 +3,7 @@ return (
     <section id="about" className=" text-center py-10">
     <h2 className="text-3xl font-bold mb-4">About Me</h2>
     <p className="text-lg font-medium">
-      I'm a beginner web developer passionate about learning and building web applications using modern technologies like TypeScript, React, and Next.js.
+      I&aposm a beginner web developer passionate about learning and building web applications using modern technologies like TypeScript React and Next.js.
     </p>
   </section>
 )
