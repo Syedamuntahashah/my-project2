70e21d51e91bd90e6fc74e77bfdde8bbec81e926
Syedamuntahashah <syedashah32399@gmail.com>
2024-11-12 14:24:41 +0500

    The second project made with next.js

--- my-portfolio2/src/app/page.tsx
+++ my-portfolio2/src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-cover bg-fixed" style={{ backgroundImage: "url('./images/images.jpg')" }}>
+    <div className="min-h-screen bg-cover bg-fixed" style={{ backgroundImage: "url('./images/images.webp')" }}>
     <Header />
 
     <div className="w-full md:w-2/3 flex flex-col space-y-6 p-4"></div>
